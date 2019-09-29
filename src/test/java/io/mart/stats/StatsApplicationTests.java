1a965d5d3f63f989241684bbd1539c9fdebedaea
Aleksandr Martiushov <aleksandr.martiushov@signavio.com>
2019-09-29 13:59:25 +0200

    Initial structure

---
+++ src/test/java/io/mart/stats/StatsApplicationTests.java
@@ -0,0 +1,16 @@
+package io.mart.stats;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StatsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
